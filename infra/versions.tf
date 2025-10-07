2890eb4881bd23fbb3a0f771affb81b384e6af96
Ramana-Raja <ramanarajakesavaraja@gmail.com>
2025-10-07 23:03:29 +0530

    CI-CD testing

--- infra/versions.tf
+++ infra/versions.tf
@@ -7,4 +7,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "terraform-state-devops-mumbai-2025-v2"
+    key     = "data-processor-v2/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
 }
